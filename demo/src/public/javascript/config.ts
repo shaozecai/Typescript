320680dd4e4fbcabd11cc1d8b2e08cea6530aa00
shaozecai <admin@shaozecai.com>
2019-08-03 08:03:27 +0800

    recoding

--- demo/src/public/javascript/config.ts
+++ demo/src/public/javascript/config.ts
@@ -7,7 +7,7 @@ interface creatConfig{
 
 let config:creatConfig = Object.create(null);
 config.timeout = 10000;
-config.baseUrl = 'http://rap2api.taobao.org/app/mock/178159';
+config.baseUrl = 'https://www.easy-mock.com/mock/5d439c9550ac9902ba740963/typescript';
 config.headers = {
     "X-Requested-With":"XMLHttpRequest",
     "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
